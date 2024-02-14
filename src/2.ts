03c1ac8dd3aae7758f65e71fe87a213cc002f773
Oksana581986 <oksanaotchych@gmail.com>
2024-02-14 19:33:29 +0200

    hw03done

--- src/2.ts
+++ src/2.ts
@@ -1,39 +1,33 @@
-/*
-  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
 
-  Клас Employee повинен включати:
+  // Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
 
-  властивість name, яка буде доступна всім.
-  властивість department, яка буде доступна лише всередині класу Employee.
-  salary, яке буде доступне лише всередині класу Employee та його підкласів.
+  // Клас Employee повинен включати:
+
+  // властивість name, яка буде доступна всім.
+  // властивість department, яка буде доступна лише всередині класу Employee.
+  // salary, яке буде доступне лише всередині класу Employee та його підкласів.
 
 
-  Клас Manager повинен бути підклас класу Employee
+  // Клас Manager повинен бути підклас класу Employee
 
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
+  // Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
 
-*/
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  class Employee {
+    constructor(public name: string, 
+      private department: string, 
+      protected salary: number) {}
+  
+    getEmployeeDetails() {
+      return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    }
   }
-
-  getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  
+  class Manager extends Employee {
+    // Реалізуйте конструктор та збільшіть salary на 10000
+    constructor(name: string, department: string, salary: number) {
+      super(name, department, salary + 10000);
+    }
   }
-}
-
-class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
-}
-
-
-export {};
+  
+  export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
+
+class Key {
+    constructor(private signature: number = Math.random()) {};
+    getSignature(): number {
+    return this.signature;
+}
+}
+
+class Person {
+    constructor( private key: Key) {}
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    constructor(
+     protected key: Key,  
+     protected door: boolean = false,
+     private tenants: Person[] = []) {}
+
+    
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log("Person came in.");
+        } else {
+            console.log("The door is closed. Person cannot come in.");
+        }
+    }
+    abstract OpenDoor(key: Key): void;
+}
+    
+class MyHouse extends House {
+        OpenDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("The door is opened.");
+        } else {
+            console.log("Invalid key. The door remains closed.");
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
+house.OpenDoor(person.getKey());
 house.comeIn(person);
-
 
 export {};
